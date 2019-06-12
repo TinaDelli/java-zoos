1e6a2b41f7fed075a94a021652da9324ea54c2eb
TinaDelli <cecurrier92@gmail.com>
2019-06-12 16:35:33 -0400

    added jackson dependency and commented out h2 scope

--- src/main/java/com/lambdaschool/zoo/models/Animal.java
+++ src/main/java/com/lambdaschool/zoo/models/Animal.java
@@ -1,5 +1,62 @@
 package com.lambdaschool.zoo.models;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "animal")
 public class Animal
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long animalid;
+
+    private String animaltype;
+
+    @ManyToMany
+    @JoinTable(name = "zooanimals",
+               joinColumns = {@JoinColumn(name = "zooid")},
+               inverseJoinColumns = {@JoinColumn(name = "animalid")})
+
+    private List<Zoo> zoos = new ArrayList<>();
+
+    public Animal()
+    {
+    }
+
+    public Animal(String animaltype)
+    {
+        this.animaltype = animaltype;
+    }
+
+    public long getAnimalid()
+    {
+        return animalid;
+    }
+
+    public void setAnimalid(long animalid)
+    {
+        this.animalid = animalid;
+    }
+
+    public String getAnimaltype()
+    {
+        return animaltype;
+    }
+
+    public void setAnimaltype(String animaltype)
+    {
+        this.animaltype = animaltype;
+    }
+
+    public List<Zoo> getZoos()
+    {
+        return zoos;
+    }
+
+    public void setZoos(List<Zoo> zoos)
+    {
+        this.zoos = zoos;
+    }
 }
